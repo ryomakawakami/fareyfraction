8186ddc11d33c196503bb83f8d768e4ff79a9564
ryomakawakami <ryomakwkm@gmail.com>
2020-07-18 19:27:07 -0400

    All the code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # fareyfraction
+
+Just Farey's algorithm to give a fraction that's approximately equal to the given decimal.
+
+```
+python -m farey
+```
+
+For example,
+
+```
+89.32841 -> 78609 / 880
+```
+
+You can mess around with the two parameters MAX_DENOM and EPSILON.
---
+++ farey/__main__.py
@@ -0,0 +1,54 @@
+import sys
+import math
+
+MAX_DENOM = 1000000
+EPSILON = 0.000001
+
+def getFraction(d):
+    numer = 0
+    denom = 0
+
+    low = [0, 1]
+    mid = [1, 2]
+    high = [1, 1]
+
+    while mid[1] < MAX_DENOM:
+        if math.fabs(mid[0] / mid[1] - d) < EPSILON:
+            return mid
+
+        if d < mid[0] / mid[1]:
+            high[0] = low[0] + mid[0]
+            high[1] = low[1] + mid[1]
+        else:
+            low[0] = mid[0] + high[0]
+            low[1] = mid[1] + high[1]
+        mid[0] = low[0] + high[0]
+        mid[1] = low[1] + high[1]
+
+    return mid
+
+def main():
+    s = input('Enter a number: ')
+    if '/' in s:
+        frac = s.split('/')
+        n = str(int(frac[0]) / int(frac[1]))
+        num = n.split('.')
+    else:
+        num = s.split('.')
+
+    if len(num) == 1:
+        print(num[0])
+    elif len(num) == 2:
+        i = int('0' + num[0])
+        d = float('0.' + num[1])
+
+        f = getFraction(d)
+
+        print(i  * f[1] + f[0], end=' / ')
+        print(f[1])
+    else:
+        print("Nope")
+        sys.exit()
+
+if __name__ == '__main__':
+    main()
